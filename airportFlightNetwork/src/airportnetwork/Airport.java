acd500e7c79a149398403051079b4f09fe04b03f
Jessica.henry <jessica.henry@dell.com>
2025-05-03 00:51:44 +0100

    airport implementation

---
+++ airportFlightNetwork/src/airportnetwork/Airport.java
@@ -0,0 +1,30 @@
+package airportnetwork;
+
+public class Airport {
+    private final String code; // DUB
+    private final String name;
+
+    public Airport(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() { return code; }
+
+    // hashCode and equals overridden for HashMap/Set usage
+    @Override
+    public int hashCode() { return code.hashCode(); }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Airport)) return false;
+        Airport other = (Airport) o;
+        return this.code.equals(other.code);
+    }
+
+    @Override
+    public String toString() {
+        return code + " (" + name + ")";
+    }
+}
