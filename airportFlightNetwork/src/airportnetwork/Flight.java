0b634331ae13a33a0da1b1ba0bda3da9df95764e
Ritika-91 <68607996+Ritika-91@users.noreply.github.com>
2025-05-12 10:24:04 +0100

    Update Flight.java

--- airportFlightNetwork/src/airportnetwork/Flight.java
+++ airportFlightNetwork/src/airportnetwork/Flight.java
@@ -1,16 +1,22 @@
 package airportnetwork;
-
+/**
+ * Represents a flight connection between two airports.
+*/
 public class Flight {
     private final Airport source;
     private final Airport destination;
     private final double weight; // Can be distance, price, etc.
-
+/**
+     * Constructs a Flight object.
+     */
     public Flight(Airport source, Airport destination, double weight) {
         this.source = source;
         this.destination = destination;
         this.weight = weight;
     }
-
+/**
+     * Gets the weight of the flight.
+     */
     public double getWeight() { return weight; }
     public Airport getDestination() { return destination; }
 }
